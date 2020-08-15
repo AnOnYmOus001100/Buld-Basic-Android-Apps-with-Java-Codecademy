c3b4773e82158a88df87523f727400206bc235e2
AnOnYmOus001100 <47621801+AnOnYmOus001100@users.noreply.github.com>
2020-08-15 13:36:32 +0530

    Android and Android Apps with java

---
+++ Android and Android Foundations/Tree.java
@@ -0,0 +1,106 @@
+/*
+	Author: AnOnYmOus001100
+	Date: 15/08/2020
+
+	Description:
+Planting a Tree
+Introduce yourself to users and plant a tree for them!
+
+Tasks:
+Creating the Class
+1.We’re starting with a blank Java file named Tree.java.
+
+Define a class that follows the Java naming conventions.
+
+
+2.This code produces an error because our program needs a main() method.
+
+Define this method inside the curly braces of the Tree class.
+
+
+
+3.Write a comment in main() that describes the task it will perform.
+
+We’re going to introduce ourselves and print a tree to the screen.
+
+You can use the single-line or multi-line syntax for your comment.
+
+
+4.Below your comment, print a message introducing yourself to the user.
+
+Try something like “Hey there, I’m Ariel and I’m learning to code in Java!”
+
+When you run the code, you should see this message printed to the screen.
+
+
+Planting Trees
+5.After introducing yourself, use another print statement to output the project goal!
+
+Something like “I’m going to plant a tree today!” or “Ready to get my hands dirty!”
+
+6.See how the second statement begins on a new line? We’ll use multiple print statements to plant our tree.
+
+For example:
+
+    System.out.println("  *  ");
+    System.out.println(" *** ");
+    System.out.println(" *** ");
+    System.out.println("  *  ");
+    System.out.println("  *  ");   
+will print a tree like this:
+
+  *  
+ *** 
+ *** 
+  *  
+  *  
+Try it out!
+
+7.Have fun and experiment with different trees.
+
+Here are a couple we made:
+
+ * ** * 
+ ****** 
+  ****  
+   **   
+   **   
+~~~~~~~~~~
+ ***** 
+ ***** 
+ ***** 
+   *   
+   *   
+#######
+You can explore other printing methods in the Java documentation.
+*/
+
+//Code:
+// Define your tree class in this file!
+class Tree{
+  public static void main(String[] args){
+    // we are going to plant a tree to the screen
+    System.out.println ("Hey AnOnYmOus001100 here.");
+    System.out.println ("plant tree on the screen using java.");
+        System.out.println ("  *  ");
+    System.out.println (" *** ");
+    System.out.println (" *** ");
+    System.out.println ("  *  ");
+    System.out.println ("  *  ");
+    System.out.println("Another tree\n");
+    System.out.println ("* ** *");
+    System.out.println ("******");
+    System.out.println (" **** ");
+    System.out.println ("  **  ");
+    System.out.println ("  **  ");
+    System.out.println ("~~~~~~");
+    System.out.println("Another tree\n");
+    System.out.println("*****");
+    System.out.println ("*****");
+    System.out.println ("*****");
+    System.out.println ("  *  ");
+    System.out.println ("  *  ");
+    System.out.println ("######");
+  
+  }
+}
